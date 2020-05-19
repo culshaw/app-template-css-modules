798556540ab86584bc5c69d538f5af9aa2701051
Zaphod Beeblebrox <zaphodbeeblebrox@zaphods-mbp-2.lan>
2020-05-19 23:03:48 +0100

    Overwrite updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import styles from "./App.module.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className={"App"}>
+    <div className={'App'}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
-        <span className={styles.Icon} />
         <a
           className="App-link"
           href="https://reactjs.org"
